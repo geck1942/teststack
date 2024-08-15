0b338bbf2072217d6ac9ec78ab78b1daa7b734f9
Aurel Jacquot <aurel@superhuit.ch>
2024-08-15 10:03:05 +0200

    chore: 🔧 updating build action after fail

--- .github/actions/tests/simple-http-test.sh
+++ .github/actions/tests/simple-http-test.sh
@@ -3,24 +3,31 @@
 # Wait for NPM run dev to start
 sleep 5
 # URL of the page to check
-URL="http://localhost:3000/blog/hello-world/"
+URL="http://localhost:3000/"
+CONTENT="Home"
 
-# Perform the HTTP request and store response
-response=$(curl --write-out "%{http_code}" --silent --output /dev/null $URL)
+# Perform the HTTP request, capturing the HTTP status and response content separately
+response=$(curl --write-out "%{http_code}" --silent --output response.txt $URL)
+response_content=$(<response.txt)
+
+# Clean up the response file
+rm response.txt
 
 # Check for HTTP status 200
 if [ "$response" -eq 200 ]; then
     echo "HTTP status is 200, checking content..."
 
-    # Fetch page content and check for the desired text
-    if curl --silent $URL | grep -q "Hello world!"; then
-        echo "Content check passed. Page contains 'Hello world!'"
+    # Check if the content contains the desired text
+    if echo "$response_content" | grep -q $CONTENT; then
+        echo "Content check passed. Page contains '$CONTENT'"
         exit 0 # Success
     else
-        echo "Content check failed. Page does not contain 'Hello world!'"
+        echo "Content check failed. Page does not contain '$CONTENT'"
         exit 1 # Fail
     fi
 else
     echo "HTTP status is not 200. Received status code: $response"
+    echo "$response_content"  # Optionally, show response content for other statuses
     exit 1 # Fail
 fi
+
